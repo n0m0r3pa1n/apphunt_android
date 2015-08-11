397e5aad94d2aa96778531c1471ccfff1a06f73e
Georgi Mirchev <gmirchev90@gmail.com>
2015-08-11 10:07:19 +0300

    Fixed imports

--- app/src/main/java/com/apphunt/app/auth/FacebookLoginProvider.java
+++ app/src/main/java/com/apphunt/app/auth/FacebookLoginProvider.java
@@ -2,14 +2,8 @@ package com.apphunt.app.auth;
 
 import android.app.Activity;
 
-import com.apphunt.app.api.apphunt.models.users.User;
 import com.facebook.AccessToken;
-import com.facebook.FacebookSdk;
 import com.facebook.login.LoginManager;
-import com.google.android.gms.common.Scopes;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.Scope;
-import com.google.android.gms.plus.Plus;
 
 /**
  * Created by nmp on 15-8-10.
