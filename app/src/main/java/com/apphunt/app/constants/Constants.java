103db8044656018a7818e2918c0928fc9f7b202b
Georgi Mirchev <gmirchev90@gmail.com>
2015-09-04 14:45:27 +0300

    Fixed constants file

--- app/src/main/java/com/apphunt/app/constants/Constants.java
+++ app/src/main/java/com/apphunt/app/constants/Constants.java
@@ -8,14 +8,11 @@ import java.util.HashMap;
 public class Constants {
     public static final String PACKAGE_NAME = "com.apphunt.app";
 
-<<<<<<< HEAD
     //TODO: use production url before release
 //    public static final String BASE_URL = "http://apphunt.herokuapp.com";
-    public static final String BASE_URL = "http://apphunt-dev.herokuapp.com";
-=======
+
     public static final String BASE_URL = "http://apphunt.herokuapp.com";
     //public static final String BASE_URL = "http://apphunt-dev.herokuapp.com";
->>>>>>> dev
 
     // User's Keys
     public static final String KEY_USER_ID = "user_id";
