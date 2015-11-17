33a31cda2aa33d4e9312d03caf224401744495fc
Georgi Mirchev <gmirchev90@gmail.com>
2015-11-17 08:26:30 +0200

    Comment service to run every day at 09 pm. Still we have to decide if we want to let the users leave comments for uninstalled apps or only for installed ones

---
+++ app/src/main/java/com/apphunt/app/services/CommentService.java
@@ -0,0 +1,232 @@
+package com.apphunt.app.services;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.android.volley.Response;
+import com.apphunt.app.MainActivity;
+import com.apphunt.app.R;
+import com.apphunt.app.api.apphunt.clients.rest.ApiClient;
+import com.apphunt.app.api.apphunt.models.apps.Packages;
+import com.apphunt.app.api.apphunt.models.notifications.Notification;
+import com.apphunt.app.api.apphunt.models.notifications.NotificationType;
+import com.apphunt.app.constants.Constants;
+import com.apphunt.app.constants.TrackingEvents;
+import com.apphunt.app.db.models.ClickedApp;
+import com.apphunt.app.db.models.InstalledApp;
+import com.apphunt.app.utils.FlurryWrapper;
+import com.apphunt.app.utils.PackagesUtils;
+import com.apphunt.app.utils.SharedPreferencesHelper;
+import com.apphunt.app.utils.ui.NotificationsUtils;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+import io.realm.RealmResults;
+
+/**
+ * Created by nmp on 15-11-17.
+ */
+public class CommentService extends Service {
+    public static final String TAG = InstallService.class.getSimpleName();
+
+    private Realm realm;
+
+    private List<ApplicationInfo> data;
+    private RealmResults<InstalledApp> dailyInstalledApps;
+    private RealmResults<ClickedApp> clickedApps;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        SharedPreferencesHelper.init(this);
+        RealmConfiguration realmConfiguration = new RealmConfiguration.Builder(this).deleteRealmIfMigrationNeeded().build();
+        realm = Realm.getInstance(realmConfiguration);
+        if (!SharedPreferencesHelper.getBooleanPreference(Constants.IS_INSTALL_NOTIFICATION_ENABLED, true)) {
+            return START_FLAG_RETRY;
+        }
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.MILLISECOND, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+
+        Date today = new Date(calendar.getTimeInMillis());
+        calendar.set(Calendar.DAY_OF_MONTH, calendar.get(Calendar.DAY_OF_MONTH) + 1);
+
+        Date tomorrow = new Date(calendar.getTimeInMillis());
+
+        dailyInstalledApps = realm.where(InstalledApp.class)
+                .between("dateInstalled", today, tomorrow)
+                .equalTo("hasNotificationShowed", false)
+                .findAll();
+
+        clickedApps = realm.where(ClickedApp.class)
+                .between("dateClicked", today, tomorrow)
+                .findAll();
+
+        if (dailyInstalledApps.size() == 0 || clickedApps.size() == 0) {
+            return START_FLAG_RETRY;
+        }
+        //TODO: Decide if we want to let users comment bad apps or only good apps (which are still currently available on their phone)
+        List<ApplicationInfo> currentlyExistingApps = PackagesUtils.getInstance().getInstalledPackages(getPackageManager());
+        //List<InstalledApp> appsInstalledTodayFromAppHunt =
+        for (int i = 0; i < dailyInstalledApps.size(); i++) {
+            InstalledApp app = dailyInstalledApps.get(i);
+            for (int j = 0; j < currentlyExistingApps.size(); j++) {
+                ApplicationInfo applicationInfo = currentlyExistingApps.get(j);
+                if(app.getPackageName().equals(applicationInfo.packageName)) {
+
+                }
+            }
+        }
+
+
+        ApiClient.getClient(this).filterApps(getInstalledPackageNames(), new Response.Listener<Packages>() {
+            @Override
+            public void onResponse(Packages response) {
+                displayNotificationForInstalledApp(response);
+            }
+        });
+
+        return START_FLAG_RETRY;
+    }
+
+
+
+    public static void setupService(Context context) {
+        AlarmManager alarmMgr = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(context, InstallService.class);
+        PendingIntent alarmIntent = PendingIntent.getService(context, Constants.RC_INSTALL_SERVICE, intent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+
+        if (alarmMgr == null) {
+            Log.d(TAG, "AlarmMgr is null");
+            return;
+        }
+
+        if (isInstallNotificationDisabled()) {
+            alarmMgr.cancel(alarmIntent);
+            return;
+        }
+
+        Calendar today = Calendar.getInstance();
+
+        Calendar calendar = Calendar.getInstance();
+
+        calendar.set(Calendar.HOUR_OF_DAY, 21);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+
+        if (today.after(calendar)) {
+            calendar.add(Calendar.DATE, +1);
+        }
+
+        alarmMgr.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
+                AlarmManager.INTERVAL_DAY, alarmIntent);
+    }
+
+    @NonNull
+    private static Boolean isInstallNotificationDisabled() {
+        return !SharedPreferencesHelper.getBooleanPreference(Constants.IS_INSTALL_NOTIFICATION_ENABLED, true);
+    }
+
+
+    public void displayNotificationForInstalledApp(Packages event) {
+        if (event.getAvailablePackages() != null &&
+                event.getAvailablePackages().size() > 0) {
+
+            InstalledApp app = findDailyInstalledAppInAvailableApps(event);
+
+            if (app == null) {
+                return;
+            }
+
+            FlurryWrapper.logEvent(TrackingEvents.UserViewedAddAppToAppHuntNotification);
+            updateNotificationDisplayed(app);
+
+            ApplicationInfo applicationInfo = PackagesUtils.getApplicationInfo(getPackageManager(), app.getPackageName());
+            String appName = applicationInfo == null ? "" : applicationInfo.loadLabel(getPackageManager()).toString();
+            Bundle bundle = new Bundle();
+            bundle.putString(Constants.EXTRA_APP_PACKAGE, app.getPackageName());
+            NotificationsUtils.displayNotification(this, MainActivity.class,
+                    bundle,
+                    new Notification("Did you like " + appName + "?",
+                            "Share your opinion with the AppHunt community!", "", NotificationType.INSTALL),
+                    BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher));
+        }
+    }
+
+    @Nullable
+    private InstalledApp findDailyInstalledAppInAvailableApps(Packages event) {
+        InstalledApp app = null;
+        List<ApplicationInfo> availablePackages = getAvailablePackages(event);
+        for (ApplicationInfo availableAppInfo : availablePackages) {
+            for (InstalledApp dailyApp : dailyInstalledApps) {
+                if (dailyApp.getPackageName().equals(availableAppInfo.packageName)) {
+                    app = dailyApp;
+                    break;
+                }
+            }
+            if (app != null) {
+                break;
+            }
+        }
+        return app;
+    }
+
+    private List<ApplicationInfo> getAvailablePackages(Packages packages) {
+        List<ApplicationInfo> tempData = new ArrayList<>();
+
+        for (ApplicationInfo info : data) {
+            for (String packageName : packages.getAvailablePackages()) {
+                if (info.packageName.equals(packageName)) {
+                    tempData.add(info);
+                }
+            }
+        }
+
+        return tempData;
+    }
+
+    private void updateNotificationDisplayed(InstalledApp app) {
+        realm.beginTransaction();
+        InstalledApp installedApp = realm.where(InstalledApp.class)
+                .equalTo("packageName", app.getPackageName())
+                .findFirst();
+        installedApp.setHasNotificationShowed(true);
+        realm.commitTransaction();
+    }
+
+
+
+    private Packages getInstalledPackageNames() {
+        Packages packages = new Packages();
+        for (ApplicationInfo info : data) {
+            packages.getPackages().add(info.packageName);
+        }
+
+        return packages;
+    }
+}
+
