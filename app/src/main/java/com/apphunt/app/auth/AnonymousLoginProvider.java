7e1994aafb0472c2941e39fe84a411e2e9ea4b54
Georgi Mirchev <gmirchev90@gmail.com>
2015-12-05 10:24:28 +0200

    Added unique id when advertising is not available.

--- app/src/main/java/com/apphunt/app/auth/AnonymousLoginProvider.java
+++ app/src/main/java/com/apphunt/app/auth/AnonymousLoginProvider.java
@@ -1,13 +1,18 @@
 package com.apphunt.app.auth;
 
 import android.content.Context;
+import android.provider.Settings;
+import android.telephony.TelephonyManager;
 import android.text.TextUtils;
+import android.util.Log;
 
 import com.apphunt.app.api.apphunt.models.users.LoginType;
 import com.apphunt.app.api.apphunt.models.users.User;
 import com.apphunt.app.constants.Constants;
 import com.apphunt.app.utils.GoogleUtils;
 import com.apphunt.app.utils.SharedPreferencesHelper;
+
+import java.util.Arrays;
 
 /**
  * * Created by Seishin <atanas@naughtyspirit.co>
@@ -38,6 +43,11 @@ public class AnonymousLoginProvider extends BaseLoginProvider {
             GoogleUtils.obtainAdvertisingId(context, new GoogleUtils.OnResultListener() {
                 @Override
                 public void onResultReady(String advertisingId) {
+                    if(TextUtils.isEmpty(advertisingId)) {
+                        advertisingId = getDeviceId(context);
+                        Log.d(TAG, "onResultReady: " + advertisingId);
+                    }
+
                     anonymousUser.setAdvertisingId(advertisingId);
                     AnonymousLoginProvider.super.login(anonymousUser);
                 }
@@ -45,6 +55,77 @@ public class AnonymousLoginProvider extends BaseLoginProvider {
         } else {
             AnonymousLoginProvider.super.login(anonymousUser);
         }
+    }
+
+    public String getDeviceId(Context context) {
+        String id = getUniqueID(context);
+        if (id == null)
+            id = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ANDROID_ID);
+        return id;
+    }
+
+    private String getUniqueID(Context context) {
+
+        String telephonyDeviceId = "NoTelephonyId";
+        String androidDeviceId = "NoAndroidId";
+
+        // get telephony id
+        try {
+            final TelephonyManager tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+            telephonyDeviceId = tm.getDeviceId();
+            if (telephonyDeviceId == null) {
+                telephonyDeviceId = "NoTelephonyId";
+            }
+        } catch (Exception e) {
+        }
+
+        // get internal android device id
+        try {
+            androidDeviceId = android.provider.Settings.Secure.getString(context.getContentResolver(),
+                    android.provider.Settings.Secure.ANDROID_ID);
+            if (androidDeviceId == null) {
+                androidDeviceId = "NoAndroidId";
+            }
+        } catch (Exception e) {
+
+        }
+
+        // build up the uuid
+        try {
+            String id = getStringIntegerHexBlocks(androidDeviceId.hashCode())
+                    + "-"
+                    + getStringIntegerHexBlocks(telephonyDeviceId.hashCode());
+
+            return id;
+        } catch (Exception e) {
+            return "0000-0000-1111-1111";
+        }
+    }
+
+    public String getStringIntegerHexBlocks(int value) {
+        String result = "";
+        String string = Integer.toHexString(value);
+
+        int remain = 8 - string.length();
+        char[] chars = new char[remain];
+        Arrays.fill(chars, '0');
+        string = new String(chars) + string;
+
+        int count = 0;
+        for (int i = string.length() - 1; i >= 0; i--) {
+            count++;
+            result = string.substring(i, i + 1) + result;
+            if (count == 4) {
+                result = "-" + result;
+                count = 0;
+            }
+        }
+
+        if (result.startsWith("-")) {
+            result = result.substring(1, result.length());
+        }
+
+        return result;
     }
 
     @Override
